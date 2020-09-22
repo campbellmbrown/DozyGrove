cdb2fdb77ea89628c64c64cb5f531e1401d8a57d
Campbell Brown <campbellbrown093@gmail.com>
2020-09-23 09:20:37 +1200

    Introduced plants

---
+++ DozyGrove/Models/Plant.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DozyGrove.Models
+{
+    public class Plant
+    {
+        protected Texture2D texture { get; set; }
+        protected string plantType { get; set; }
+        protected Rectangle textureRectangle { get; set; }
+        protected int stage { get; set; }
+        protected int maxStage { get; set; }
+
+        public Dictionary<string, int> maxStages = new Dictionary<string, int>()
+        {
+            { "weed", 1 },
+            { "tomato", 6 }
+        };
+
+        public Dictionary<int, Rectangle> tomatoAssignments = new Dictionary<int, Rectangle>()
+        {
+            { 1, new Rectangle(0 * Tile.tileSize, Tile.tileSize, Tile.tileSize, Tile.tileSize) },
+            { 2, new Rectangle(1 * Tile.tileSize, Tile.tileSize, Tile.tileSize, Tile.tileSize) },
+            { 3, new Rectangle(2 * Tile.tileSize, Tile.tileSize, Tile.tileSize, Tile.tileSize) },
+            { 4, new Rectangle(3 * Tile.tileSize, Tile.tileSize, Tile.tileSize, Tile.tileSize) },
+            { 5, new Rectangle(4 * Tile.tileSize, Tile.tileSize, Tile.tileSize, Tile.tileSize) },
+            { 6, new Rectangle(5 * Tile.tileSize, Tile.tileSize, Tile.tileSize, Tile.tileSize) },
+        };
+
+        public Plant(Texture2D texture, string plantType)
+        {
+            this.texture = texture;
+            this.plantType = plantType;
+            stage = 1;
+            maxStage = maxStages[plantType];
+            UpdateRectangle();
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            spriteBatch.Draw(texture, position, textureRectangle, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+
+        public void Grow()
+        {
+            if (stage < maxStage)
+                stage++;
+            UpdateRectangle();
+        }
+
+        public void UpdateRectangle()
+        {
+            switch (plantType)
+            {
+                case "weed":
+                    textureRectangle = new Rectangle(0, 0, Tile.tileSize, Tile.tileSize);
+                    break;
+                case "tomato":
+                    textureRectangle = tomatoAssignments[stage];
+                    break;
+            }
+        }
+    }
+}
--- DozyGrove/Sprites/Soil.cs
+++ DozyGrove/Sprites/Soil.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+﻿using DozyGrove.Models;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +12,29 @@ namespace DozyGrove.Sprites
 {
     public class Soil : InteractiveSprite
     {
+        public Plant plant;
+        public bool hasPlant { get { return plant != null; } }
+
         public Soil() : base(Game1.textures["soil"])
         {
+            if (Game1.r.Next(1, 101) <= 20)
+                plant = new Plant(Game1.textures["plants"], "tomato");
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            base.Draw(spriteBatch, position);
+            if (hasPlant)
+                plant.Draw(spriteBatch, position);
+        }
+
+        public override void DailyUpdate()
+        {
+            if (!hasPlant && Game1.r.Next(1, 101) <= 5)
+                plant = new Plant(Game1.textures["plants"], "weed");
+            if (hasPlant)
+                plant.Grow();
+            base.DailyUpdate();
         }
     }
 }
